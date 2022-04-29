075bf9297abeeaed12d87f0f1c7aeadbd2babf5a
MayraAhuja <90937554+MayraAhuja@users.noreply.github.com>
2022-04-29 17:13:36 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload() {
+    
+}
+
+
+function setup() {
+ canvas= createCanvas(700, 520);
+ canvas.center();
+}
+
+
+function draw() {
+    
+}
+
+
+function saveIMG() {
+    save("new me and new you.png");
+}
